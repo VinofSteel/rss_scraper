8cb02b7202b48b62065f0fa121fac244e74321be
VinofSteel <v.nascimento23@yahoo.com.br>
2024-06-07 00:31:30 -0300

    refactor: changing users table creation to auto generate the uuids instead of having to insert them manunally

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -1,10 +1,12 @@
 -- +goose Up
-CREATE TABLE users (
-    id UUID PRIMARY KEY,
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
     name TEXT NOT NULL
 );
 
 -- +goose Down
-DROP TABLE users;
+DROP TABLE IF EXISTS users;
